78ca396c0f8559412ed1880a3c18aa970247de46
md82680 <mdespo@gmail.com>
2024-09-03 15:09:43 -0400

    Initial file commit for NextJS project

---
+++ src/pages/posts/first-post.js
@@ -0,0 +1,10 @@
+import React from "react";
+function FirstPostPage() {
+  return (
+    <div>
+      <h1>First Post</h1>
+      <p>This is my first post</p>
+    </div>
+  );
+}
+export default FirstPostPage;
